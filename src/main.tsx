7fd117b335e34e28947ae941f8db0d9be8cf62fb
Dmitry Sabun <dmitry_sabun@epam.com>
2026-03-20 10:19:13 -0700

    Analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { TimeProvider } from './context/TimeContext.tsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TimeProvider>
       <App />
+      <Analytics />
     </TimeProvider>
   </StrictMode>,
 )
